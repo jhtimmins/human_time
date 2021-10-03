96442f3718014364dfabd13b1c0aff07a083df60
James Timmins <jameshtimmins@gmail.com>
2021-10-03 14:20:29 -0700

    Added setuptools.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="human_time",
+    version="0.0.1",
+    description="Makes Datetimes human-readable",
+    author="James Timmins",
+    author_email="",
+    url="https://github.com/jhtimmins/human_time",
+    install_requires=[],
+    packages=find_packages(),
+    entry_points={},
+    py_modules=["human_time"],
+)
