be32abf83ed50fb47f115103b0964d4e6ab241a3
James Timmins <jameshtimmins@gmail.com>
2021-10-03 15:11:34 -0700

    Move to src file.

--- setup.py
+++ setup.py
@@ -2,15 +2,26 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="human_time",
     version="0.0.1",
     description="Makes Datetimes human-readable",
+    long_description=long_description,
     author="James Timmins",
-    author_email="",
+    author_email="jameshtimmins@gmail.com",
     url="https://github.com/jhtimmins/human_time",
     install_requires=[],
-    packages=find_packages(),
+    packages=setuptools.find_packages(where="src"),
     entry_points={},
     py_modules=["human_time"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6",
 )
